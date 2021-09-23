52464277766603c054df7710b01a1a0bb14cbb7e
michaelhm <mclinden.michael@gmail.com>
2021-09-23 15:23:17 -0400

    rename to math_functions Add unit tests for all number types

--- math_lib/math_functions.py
+++ math_lib/math_functions.py
@@ -1,10 +1,11 @@
 from math import sqrt
 from operator import add, sub, mul, truediv, pow
 
-subtract=sub
-multiply=mul
-divide=truediv
-exponent=pow
+subtract = sub
+multiply = mul
+divide = truediv
+exponent = pow
+power = pow
 
 def math_operations(x, operator, y=False):
     if (operator == sqrt):
--- tests/test_functions.py
+++ tests/test_functions.py
@@ -1,21 +1,52 @@
-from math_lib import functions
+from math_lib import math_functions
 from operator import add, sub, mul, truediv, pow
 from math import sqrt
 
+math_func = math_functions.math_operations
+
 def test_math_operations_add():
-    assert functions.math_operations(3, add, 4) == 7
+    testInteger = math_func(2, add, 3)
+    testBinary = math_func(0b10, add, 0b11)
+    testHexadecimal = math_func(0x02, add, 0x03)
+    testOctal = math_func(0o02, add, 0o03)
+    testSelf = math_func(2, add)
+    assert (testInteger, testBinary, testHexadecimal, testOctal, testSelf) == (5, 5, 5, 5, 4)
 
 def test_math_operations_subtract():
-    assert functions.math_operations(4, sub, 3) == 1
+    testInteger = math_func(3, sub, 2)
+    testBinary = math_func(0b11, sub, 0b10)
+    testHexadecimal = math_func(0x03, sub, 0x02)
+    testOctal = math_func(0o03, sub, 0o02)
+    testSelf = math_func(2, sub)
+    assert (testInteger, testBinary, testHexadecimal, testOctal, testSelf) == (1, 1, 1, 1, 0)
 
 def test_math_operations_divide():
-    assert functions.math_operations(6, truediv, 3) == 2
+    testInteger = math_func(6, truediv, 3)
+    testBinary = math_func(0b110, truediv, 0b11)
+    testHexadecimal = math_func(0x06, truediv, 0x03)
+    testOctal = math_func(0o06, truediv, 0o03)
+    testSelf = math_func(2, truediv)
+    assert (testInteger, testBinary, testHexadecimal, testOctal, testSelf) == (2, 2, 2, 2, 1)
 
 def test_math_operations_multiply():
-    assert functions.math_operations(2, mul, 3) == 6
+    testInteger = math_func(2, mul, 3)
+    testBinary =  math_func(0b10, mul, 0b11)
+    testHexadecimal = math_func(0x02, mul, 0x03)
+    testOctal = math_func(0o03, mul, 0o02)
+    testSelf = math_func(2, mul)
+    assert (testInteger, testBinary, testHexadecimal, testOctal, testSelf) ==(6, 6, 6, 6, 4)
 
 def test_math_operations_exponent():
-    assert functions.math_operations(2, pow, 3) == 8
+    testInteger = math_func(2, pow, 3)
+    testBinary = math_func(0b10, pow, 0b11)
+    testHexadecimal = math_func(0x02, pow, 0x03)
+    testOctal = math_func(0o02, pow, 0o03)
+    testSelf = math_func(2, pow)
+    assert (testInteger, testBinary, testHexadecimal, testOctal, testSelf) == (8, 8, 8, 8, 4)
 
 def test_math_operations_sqrt():
-    assert functions.math_operations(4, sqrt) == 2
+    testInteger = math_func(4, sqrt)
+    testBinary = math_func(0b100, sqrt)
+    testHexadecimal = math_func(0x04, sqrt)
+    testOctal = math_func(0o04, sqrt)
+    assert (testInteger, testBinary, testHexadecimal, testOctal) == (2, 2, 2, 2)
